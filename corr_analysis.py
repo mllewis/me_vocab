47a5bde5a040490cf23025e952d8ea718b49e3eb
brendenlake <brenden.lake@gmail.com>
2017-06-13 15:25:05 -0400

    analysis as of 6/12/17

--- corr_analysis.py
+++ corr_analysis.py
@@ -5,7 +5,6 @@ import csv
 from scipy import stats
 
 # Analysis for predicting vocab level by ME strength
-
 def get_acc_by_user(df,field_correct,code_correct):
  	# Compute accuracy across a task for each user
 	#
@@ -19,7 +18,9 @@ def get_acc_by_user(df,field_correct,code_correct):
 	trial_acc = {}	
 	for myuser in users:
 		
-		trials = np.array( df.loc[(df.SubjectID == myuser), field_correct] )
+		trials = df.loc[(df.SubjectID == myuser), field_correct]
+		trials = np.array( trials , dtype=str)		
+		trials = trials[trials != 'nan'] # remove incomplete trials
 		assert len(set(trials))<=2 # make sure this is a binary response
 		mycorrect = trials == code_correct
 		trial_count[myuser] = np.size(mycorrect)
@@ -27,20 +28,43 @@ def get_acc_by_user(df,field_correct,code_correct):
 
 	return trial_acc,trial_count
 
+def exclude_practice(df):
+	# Exclude the practice items in the vocab app.
+	# This may not be necessary is importing data from Flurry logs, as the preprocess.py script also has a function for this.
+	# This is necessary if important data from iPad logs.
+	to_exclude = ['dog','ball'] # the practice items
+	nitems = df.shape[0]
+	for ex in to_exclude:
+		df = df[df.Object_Asked != ex]
+	ndrop = nitems - df.shape[0]
+	print "Excluding practice trials..."
+	print "  dropping " + str(ndrop) + " practice items"
+	print ""
+	return df
+
 if __name__ == "__main__":
 
 	# Parameters
-	fn_in_fendle_processed = 'data-flurry/fendle-processed-12-9-16.csv' # input data from fendle
-	fn_in_vocab_processed = 'data-flurry/vocab-processed-12-9-16.csv'
-	fn_in_ages = 'data-flurry/ages-12-9-16.csv' # list of user names and ages
+	# fn_in_fendle_processed = 'data-flurry/fendle-processed-12-9-16.csv' # input data from fendle
+	# fn_in_vocab_processed = 'data-flurry/vocab-processed-12-9-16.csv'
+	# fn_in_ages = 'data-flurry/ages-12-9-16.csv' # list of user names and ages
+
+	# fn_in_fendle_processed = 'data-flurry/fendle-iPad-processed-2-1-17.csv' # input data from fendle
+	# fn_in_vocab_processed = 'data-flurry/vocab-device-results-2-7-17.csv'
+	# fn_in_ages = 'data-flurry/ages-2-14-17.csv' # list of user names and ages
+
+	fn_in_fendle_processed = 'data-flurry/fendle-processed-06-12-17.csv' # input data from fendle
+	fn_in_vocab_processed = 'data-flurry/vocab-device-results-06-12-17.csv'
+	fn_in_ages = 'data-flurry/ages-06-12-17.csv' # list of user names and ages
 
 	ntrial_fendle = 19 # full number of trials in an experiment
 	ntrial_vocab = 20
 
-	fn_out_summary = 'data-flurry/summary-12-9-16.csv' # output summary of data
+	fn_out_summary = 'data-flurry/summary-06-12-17.csv' # output summary of data
 
-	exclude_user = ['1191602', '1191605', '1191606', '11161604', '1261606', '1281601']
-		# from Veronica's spreadsheet
+	# exclude_user = ['1191602', '1191605', '1191606', '11161604', '1261606', '1281601'] # from December 2016
+ 	# exclude_user = ["1191602","1191605","1191606","11161604","1261606","1281601","12131602","01031704","01031705","01051703","01061701","01061702","01061703","01061706","01121705","01121707","01121708","01131703","01171701","01171703","01191701","01191702","01191704","01191706","01271701","01311701","Test"] # from Feb. 2017
+ 	exclude_user = ["1191602","1191605","1191606","11161604","1261606","1281601","12131602","01031704","01031705","01051703","01061701","01061702","01061703","01061706","01121705","01121707","01131703","01171701","01171703","01191701","01191702","01191704","01191706","01271701","01311701","02071703","02071705","02071706","02091701","02201708","02201709","02201712","02201713","02281703","03021701","03211701","03301701","04111706","04111707","04111708","04181701","05241702","05241704","06021701","06041701","06041702","06041704","06041705","06041708","06041709","06041713","06091702","Test"]
 
 	# Main
 	print ""
@@ -48,17 +72,28 @@ if __name__ == "__main__":
 	print ""
 	df_fendle = pd.read_csv(fn_in_fendle_processed)
 	df_vocab  = pd.read_csv(fn_in_vocab_processed)
+	df_vocab = exclude_practice(df_vocab)
 
 	acc_fendle,count_fendle = get_acc_by_user(df_fendle, field_correct="CorrectFirstTry?", code_correct='Y')
-	acc_vocab,count_vocab = get_acc_by_user(df_vocab, field_correct="Correct", code_correct="Yes")	
-	with open(fn_in_ages, 'rb') as csvfile:
-		myreader = csv.reader(csvfile)
-		my_ages = {pairs[0] : float(pairs[1]) for idx,pairs in enumerate(myreader) if idx > 0} # skip header
+	acc_vocab,count_vocab = get_acc_by_user(df_vocab, field_correct="Correct", code_correct="Yes")
+	
+	# get ages
+	df_age = pd.read_csv(fn_in_ages)
+	v_ids = df_age['SubjectID'].as_matrix()
+	v_ages = df_age['Age'].as_matrix()
+	my_ages = {v_ids[i] : v_ages[i] for i in range(len(v_ids)) }
+	# with open(fn_in_ages, 'rb') as csvfile:
+	# 	myreader = csv.reader(csvfile)
+	# 	my_ages = {pairs[0] : float(pairs[1]) for idx,pairs in enumerate(myreader) if idx > 0} # skip header
 
 	print "Find participants that have data in both experiments... "
-	print ""
+	print "  N = " + str(len(set(acc_fendle.keys()))) + " for Fendle app"
+	print "  N = " + str(len(set(acc_vocab.keys()))) + " for vocab app"
+	print "  excluding " + str(len(exclude_user)) + " participants"
 	user_with_both = ( set(acc_fendle.keys()) & set(acc_vocab.keys()) ) - set(exclude_user)
 	user_with_both = sorted(list(user_with_both))
+	print "  leaving N = " + str(len(user_with_both)) + " total"
+	print ""
 
 	print "Flagging participants with unusual number of trials... "
 	mypass = True
@@ -88,10 +123,10 @@ if __name__ == "__main__":
 	print "Computing simple correlations... "
 	n = len(v_acc_vocab)
 	(r,p) = stats.pearsonr(v_acc_fendle,v_acc_vocab)
-	print "  correlation between vocab and ME is r = " + str(round(r,3)) + ", p = " + str(round(p,3)) + " (n=" + str(n) + ")"
+	print "  correlation between vocab and ME is r = " + str(round(r,3)) + ", p = " + str(round(p,8)) + " (n=" + str(n) + ")"
 
 	(r,p) = stats.pearsonr(v_age,v_acc_vocab)
-	print "  correlation between vocab and age is r = " + str(round(r,3)) + ", p = " + str(round(p,3)) + " (n=" + str(n) + ")"
+	print "  correlation between vocab and age is r = " + str(round(r,3)) + ", p = " + str(round(p,8)) + " (n=" + str(n) + ")"
 	print ""
 
 	print "Writing summary of data to CSV file '" + str(fn_out_summary) +  "'... "
@@ -102,21 +137,21 @@ if __name__ == "__main__":
 		fid.writerow( [user_with_both[idx],v_age[idx],v_acc_vocab[idx],v_acc_fendle[idx],v_count_vocab[idx],v_count_fendle[idx]] )
 
 
-	print "Scatter plots... "	
+	print "Scatter plots... "
 	plt.figure(1,figsize=(8,4))
 	plt.clf()
 
 	plt.subplot(1,2,1)
-	plt.scatter(v_acc_fendle,v_acc_vocab)
-	plt.xlabel('ME (accuracy)')
-	plt.ylabel('vocab (accuracy)')
+	plt.scatter(v_acc_fendle,v_acc_vocab,alpha=0.3)
+	plt.xlabel('Mutual Exclusivity strength (% correct)')
+	plt.ylabel('Vocabulary strength (% correct)')
 	plt.xlim((30,105))
 	plt.ylim((0,105))
 
 	plt.subplot(1,2,2)
-	plt.scatter(v_age,v_acc_vocab)
+	plt.scatter(v_age,v_acc_vocab,alpha=0.3)
 	plt.ylim((0,105))
-	plt.xlabel('age')
+	plt.xlabel('Age (in years)')
 	# plt.ylabel('vocab (accuracy)')
 	plt.show()
 	print ""
--- preprocess.py
+++ preprocess.py
@@ -9,8 +9,12 @@ def parse_lines(fn, key_trial):
 	#  fn : file name
 	#  key_trial : presence of this 'field' in JSON is necessary and sufficient condition for a 'trial'
 
-	with open(fn, "rb") as fid:
+	with open(fn, "rb") as fid: # for files with end of line markers
 		lines = [l.strip('\n') for l in fid.readlines()]
+	if len(lines)==1: # for files without end of line markers
+		with open(fn, "rb") as fid: #reread
+			lines = fid.read().splitlines()
+	nline = len(lines)
 
 	# only include lines with embedded JSON
 	lines = [l for l in lines if '{' in l and '}' in l]
@@ -27,11 +31,21 @@ def parse_lines(fn, key_trial):
 	# convert each trial into a dictionary
 	trials = []
 	for myline in lines:
-		pairs = [p.split(':',1) for p in myline] # split into key and value
+
+		pairs = myline
+
+		# necessary to make field style v2 compatible with v1 (v2 uses quotes around key/value pairs)
+		pairs = [p.replace('"','') for p in pairs] # remove quotes from both key and values
+		pairs = [p.replace('Subject_ID','Subject ID') for p in pairs] # this field was renamed
+		
+		# parse the fields 
+		pairs = [p.split(':',1) for p in pairs] # split into key and value		
 		pairs = { p[0].replace(' ','') : p[1].strip() for p in pairs} # remove spaces from key-name
+		# mykeys = [key for key in pairs if 'Correct' in key]
 		if key_trial in pairs: # check if this is a trial
 			trials.append(pairs)
-
+			
+	print "  found " + str(len(trials)) + " trials in " + str(nline) + " lines of raw logs"
 	return trials
 
 def exclude_trials(trials,key,value):
@@ -41,6 +55,17 @@ def exclude_trials(trials,key,value):
 	if len(trials_new) < len(trials):
 		print "  removed " + str(len(trials) - len(trials_new)) + " trials with {"  + key + " = " + value + "}"
 	return trials_new
+
+def rename_subject(trials,key,new_name,old_name):
+	flag = True
+	for t in trials:
+		if t[key] == old_name:
+			t[key] = new_name
+			if flag:
+				print "  Participant " + old_name + " renamed as " + new_name + " to correct incorrect entry."
+				print ""
+				flag = False
+	return trials
 
 def make_simple_csv(fn_out,trials,columns):
 	# Write to CSV with row for each trial
@@ -56,7 +81,7 @@ def make_simple_csv(fn_out,trials,columns):
 			if c not in t:
 				t[c] = 'NA'
 
-	# write to CSV
+	# write to CSV`
 	fid = csv.writer(open(fn_out, "wb"), quoting=csv.QUOTE_ALL)
 	fid.writerow(columns) # header
 	for t in trials:
@@ -65,26 +90,49 @@ def make_simple_csv(fn_out,trials,columns):
 if __name__ == "__main__":
 
 	# Parameters
-	fn_in_fendle = 'data-flurry/fendle-12-9-16.csv'
-	fn_out_fendle = 'data-flurry/fendle-processed-12-9-16.csv'
+	# fn_in_fendle = 'data-flurry/fendle-12-9-16.csv'
+	# fn_out_fendle = 'data-flurry/fendle-processed-12-9-16.csv'
+	# fields_fendle =  ['SubjectID','CorrectFirstTry?','TrialType','StageType','Object','FoxAssistance','StartTime','ResponsiveStartTime','EndTime'] # fields to include in new csv file
+
+	# fn_in_vocab = 'data-flurry/vocab-12-9-16.csv'
+	# fn_out_vocab = 'data-flurry/vocab-processed-12-9-16.csv'
+	# fields_vocab = ['SubjectID','Correct','Object_Asked','Correct_Image','Incorrect_Image'] # fields to include in new csv field
+
+	# fn_in_fendle = 'data-flurry/fendle-iPad-2-1-17.csv'
+	# fn_out_fendle = 'data-flurry/fendle-iPad-processed-2-1-17.csv'
+	# fields_fendle =  ['SubjectID','CorrectFirstTry?','TrialType','StageType','Object','FoxAssistance','StartTime','ResponsiveStartTime','EndTime'] # fields to include in new csv file
+
+	# fn_in_vocab = 'data-flurry/vocab-2-13-17.csv'
+	# fn_out_vocab = 'data-flurry/vocab-processed-2-13-17.csv'
+	# fields_vocab = ['SubjectID','Correct','Object_Asked','Correct_Image','Incorrect_Image'] # fields to include in new csv field
+
+	fn_in_fendle = 'data-flurry/fendle-06-12-17.csv'
+	fn_out_fendle = 'data-flurry/fendle-processed-06-12-17.csv'
 	fields_fendle =  ['SubjectID','CorrectFirstTry?','TrialType','StageType','Object','FoxAssistance','StartTime','ResponsiveStartTime','EndTime'] # fields to include in new csv file
 
-	fn_in_vocab = 'data-flurry/vocab-12-9-16.csv'
-	fn_out_vocab = 'data-flurry/vocab-processed-12-9-16.csv'
+	fn_in_vocab = 'data-flurry/vocab-06-12-17.csv'
+	fn_out_vocab = 'data-flurry/vocab-processed-06-12-17.csv'
 	fields_vocab = ['SubjectID','Correct','Object_Asked','Correct_Image','Incorrect_Image'] # fields to include in new csv field
 
 	print ""
-	print "Preprocessing raw data from files '" + fn_in_fendle + "' and '" + fn_in_vocab + "'...."
-	print ""
+	print "Preprocessing raw data from files '" + fn_in_fendle	
 	S_fendle = parse_lines(fn_in_fendle,key_trial='TrialType')
-	S_vocab = parse_lines(fn_in_vocab,key_trial='Correct')
-
-	print "Excluding demo trials for vocab app ...."
-	S_vocab = exclude_trials(S_vocab,'Object_Asked','dog')
-	S_vocab = exclude_trials(S_vocab,'Object_Asked','ball')
 	print ""
+	
+	# print "Preprocessing raw data from files '" + fn_in_vocab
+	# S_vocab = parse_lines(fn_in_vocab,key_trial='Correct')
+	# print ""
+
+	# rename participant
+	S_fendle = rename_subject(S_fendle,"SubjectID",new_name="01051704",old_name="01051703")
+	S_fendle = rename_subject(S_fendle,"SubjectID",new_name="02101702",old_name="02101701")
+
+	# print "Excluding demo trials for vocab app ...."
+	# S_vocab = exclude_trials(S_vocab,'Object_Asked','dog')
+	# S_vocab = exclude_trials(S_vocab,'Object_Asked','ball')
+	# print ""
 
 	print "Creating CSV files '" + fn_out_fendle + "' and '" + fn_out_vocab +  "'...."
 	print ""
 	make_simple_csv(fn_out_fendle,S_fendle,fields_fendle)
-	make_simple_csv(fn_out_vocab,S_vocab,fields_vocab)
+	# make_simple_csv(fn_out_vocab,S_vocab,fields_vocab)
